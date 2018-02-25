1dd02ccc6bc8a410964f965bdc738193526ce793
Nana Mochihara <nanahaan@gmail.com>
2018-02-25 22:03:59 +0100

    Get address and geocode from google map API

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,6 +2,9 @@ import * as React from "react";
 import SearchForm from "./SearchForm";
 import '../stylesheets/index.css';
 import GeocodeResult from "./GeocodeResult";
+import axios from "axios";
+
+const GEOCODE_ENDPOINT = 'https://maps.googleapis.com/maps/api/geocode/json';
 
 class App extends React.Component {
   constructor(props) {
@@ -12,7 +15,16 @@ class App extends React.Component {
   }
 
   handlePlaceSubmit(place) {
-    console.log(place);
+    axios.get(GEOCODE_ENDPOINT, { params: { address: place } })
+    .then(results => {
+      const result = results.data.results[0];
+      const location = result.geometry.location;
+      this.setState({
+        address: result.formatted_address,
+        lat: location.lat,
+        lng: location.lng
+      });
+    });
   }
 
   render() {
